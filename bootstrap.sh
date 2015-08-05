c565d1f58510d63fa9d269246cd955199018ee71
Mateusz Łapsa-Malawski <m@cr3.io>
2015-08-05 17:06:38 +0100

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,10 +6,12 @@ function module()
     if [ ! -e ${module_path} ]; then
         echo Downloading ${module_path}
         mkdir -p modules
-        curl -so modules/salt.sh https://raw.githubusercontent.com/ministryofjustice/opg-bootstrap/master/README.md
+        curl -so ${module_path} https://raw.githubusercontent.com/ministryofjustice/opg-bootstrap/master/${module_path}
     fi
     echo Loading ${module_path}
     source ${module_path}
 }
 
 module modules/salt.sh
+module modules/docker.sh
+module modules/volumes.sh
