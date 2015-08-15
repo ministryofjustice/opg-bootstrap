435797b25a9826847d3ac82bbbd9f9e0396e1d68
Mateusz Lapsa-Malawski <m@cr3.io>
2015-08-15 18:11:59 +0100

    fix

--- modules/20-salt.sh
+++ modules/20-salt.sh
@@ -12,6 +12,9 @@ apt-get -y --force-yes install python-pip
 pip install pyzmq m2crypto pycrypto gitpython psutil boto boto3
 pip install salt==${SALT_VERSION}
 
+mkdir -p /etc/salt
+touch /etc/salt/minion
+
 if [  "${IS_SALTMASTER}" == "yes" ]; then
     curl -o /etc/init/salt-master.conf https://raw.githubusercontent.com/saltstack/salt/develop/pkg/salt-master.upstart
     cat <<EOF >> /etc/salt/master
@@ -27,8 +30,6 @@ EOF
 fi
 
 curl -o /etc/init/salt-minion.conf https://raw.githubusercontent.com/saltstack/salt/develop/pkg/salt-minion.upstart
-mkdir -p /etc/salt
-touch /etc/salt/minion
 cat <<EOF >> /etc/salt/grains
 opg-role: ${OPG_ROLE}
 EOF
