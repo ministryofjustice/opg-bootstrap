255e41fd1f5cb417a687f03e9adbcac50bb2eca0
Mateusz Łapsa-Malawski <m@cr3.io>
2015-08-18 17:01:38 +0100

    new bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,17 @@
 #let's log the output
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
+function download()
+{
+    #try downloading a file for 5 mins
+    local module_path=$${1}
+    local retry_count_down=30
+    while ! wget --no-verbose --retry-connrefused --random-wait -O $${module_path} https://raw.githubusercontent.com/ministryofjustice/opg-bootstrap/master/$${module_path} && [ $${retry_count_down} -gt 0 ] ; do
+        retry_count_down=$((retry_count_down - 1))
+        sleep 10
+    done
+}
+
 function module()
 {
     local module_path=$${1}
@@ -20,7 +31,6 @@ readonly OPG_ROLE=${opg_role}
 
 readonly DOCKER_COMPOSE_VERSION=${docker_compose_version}
 readonly SALT_VERSION=${salt_version}
-
 
 module modules/00-start.sh
 module modules/10-volumes.sh
