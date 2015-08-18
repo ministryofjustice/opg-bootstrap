4d34d797a4405523f2e1ebe75efa39a1bd615934
Mateusz Łapsa-Malawski <m@cr3.io>
2015-08-18 15:46:55 +0100

    ship upstart scripts to restart salt-minion

--- modules/20-salt.sh
+++ modules/20-salt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-################################################### hostname install salt
+################################################### install salt
 echo Installing salt
 apt-get -y --force-yes install build-essential pkg-config swig
 apt-get -y --force-yes install libyaml-0-2 libgmp10
@@ -13,13 +13,31 @@ pip install pyzmq m2crypto pycrypto gitpython psutil boto boto3
 pip install salt==${SALT_VERSION}
 
 mkdir -p /etc/salt
-cat <<EOF >> /etc/salt/minion
-log_level: warning
-log_level_logfile: all
-EOF
+
+
+##### salt-master
 
 if [  "${IS_SALTMASTER}" == "yes" ]; then
-    curl -o /etc/init/salt-master.conf https://raw.githubusercontent.com/saltstack/salt/develop/pkg/salt-master.upstart
+    # let's' install upstart job for salt-master
+    cat <<EOF >> /etc/init/salt-master.conf
+description "Salt Master"
+
+start on (net-device-up
+          and local-filesystems
+          and runlevel [2345])
+stop on runlevel [!2345]
+limit nofile 100000 100000
+
+script
+  # Read configuration variable file if it is present
+  [ -f /etc/default/$UPSTART_JOB ] && . /etc/default/$UPSTART_JOB
+
+  # Activate the virtualenv if defined
+  [ -f $SALT_USE_VIRTUALENV/bin/activate ] && . $SALT_USE_VIRTUALENV/bin/activate
+
+  exec salt-master
+end script
+EOF
     cat <<EOF >> /etc/salt/master
 auto_accept: True
 file_roots:
@@ -32,7 +50,42 @@ EOF
     start salt-master
 fi
 
-curl -o /etc/init/salt-minion.conf https://raw.githubusercontent.com/saltstack/salt/develop/pkg/salt-minion.upstart
+
+##### salt-minion
+
+# let's' install upstart job for salt-minion
+cat <<EOF >> /etc/init/salt-minion.conf
+
+description "Salt Minion"
+
+start on (net-device-up
+          and local-filesystems
+          and runlevel [2345])
+stop on runlevel [!2345]
+
+# respawn forever
+post-stop exec sleep 10
+respawn
+respawn limit 10 5
+
+script
+  # Read configuration variable file if it is present
+  [ -f /etc/default/$UPSTART_JOB ] && . /etc/default/$UPSTART_JOB
+
+  # Activate the virtualenv if defined
+  [ -f $SALT_USE_VIRTUALENV/bin/activate ] && . $SALT_USE_VIRTUALENV/bin/activate
+
+  exec salt-minion
+end script
+EOF
+
+# salt-minon configuration
+cat <<EOF >> /etc/salt/minion
+log_level: warning
+log_level_logfile: all
+EOF
+
+# let's set grains
 cat <<EOF >> /etc/salt/grains
 opg-role: ${OPG_ROLE}
 EOF
