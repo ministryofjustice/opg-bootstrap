f0f19141203ce908cdce6936fa1c4c39d736b823
Allen Clarke <allen.clarke@transformuk.com>
2016-04-18 14:37:06 +0100

    salt-minion will now check connectivity and run a highstate on first boot

--- modules/90-salt.sh
+++ modules/90-salt.sh
@@ -182,6 +182,34 @@ file_roots:
 EOF
     salt-call --local state.highstate
 else
-    start salt-minion
+    # Check whether there is a connectivity with the
+    # Salt Master by checking both ports on which it
+    # should listen (4505 and 4506).
+    MASTER_RESPONSES=()
+
+    for n in {1..10}; do
+        for p in 4505 4506; do
+            if nc -z -w 3 salt $p &> /dev/null; then
+                MASTER_RESPONSES+=( $p )
+            fi
+        done
+
+        # Break from loop if both ports responding
+        (( ${#MASTER_RESPONSES[@]} >= 2 )) && break
+
+        sleep 1
+    done
+
+    echo ${#MASTER_RESPONSES[@]}
+
+    # Do not attempt to run the Salt highstate
+    # if the Salt Master is not responding.
+    if (( ${#MASTER_RESPONSES[@]} < 2 )); then
+        echo "Unable to contact the Salt Master, aborting..."
+        exit 1
+    fi
+
+    # Restart minion and run highstate
+    restart salt-minion
     salt-call state.highstate
 fi
