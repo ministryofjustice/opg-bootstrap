05a8d8a1562cf417ada621a4a6371acc41edc3bf
Brett Minnie <github@bdb-studios.co.uk>
2016-05-18 13:12:00 +0100

    Added domain suffix for stuff not rendering correctly

--- modules/90-salt.sh
+++ modules/90-salt.sh
@@ -168,6 +168,7 @@ opg_environment: ${OPG_ENVIRONMENT}
 opg_account_id: "${OPG_ACCOUNT_ID}"
 opg_region_id: "${OPG_REGION_ID}"
 opg_shared_suffix: "${OPG_SHARED_SUFFIX}"
+opg_domain: "${OPG_DOMAIN}"
 EOF
 
 #start salt minion service when not in standalone mode
