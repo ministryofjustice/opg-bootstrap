242fe89ba7f9dc05e20036fc9f8db15471d1c539
Tim McArthur <tim.mcarthur@digital.justice.gov.uk>
2016-07-22 10:09:27 +0100

    Update test for pillar env

--- tests/check_bootstrap.py
+++ tests/check_bootstrap.py
@@ -3,6 +3,7 @@
 # check services are running: salt-minion, salt-master, docker
 # check salt-master config
 # check minionid matches host
+# check that pillarenv is set on minion
 # check that extra volumes are mounted
 # check nfs storage is configured
 # check /etc/environment and /etc/salt/grains are set
@@ -12,23 +13,33 @@ import os
 import sys
 import subprocess
 
-class bootstrapTest(object):
 
-    def __init__(self, end_path, test_type ):
+class BootstrapTest(object):
+
+    def __init__(self, end_path, t_type):
         """
 
         :param end_path: path of file to check that bootstrap has completed
         :param test_type: type of image being tested. salt or docker are valid values
         """
         self._end_file = end_path
-        self.test_type = test_type
+        self.test_type = t_type
         self.services = {
             'salt':
-                [ 'salt-minion', 'salt-master' ],
+                ['salt-minion', 'salt-master'],
             'docker':
-                [ 'salt-minion', 'docker' ]
+                ['salt-minion', 'docker']
         }
         self.service_state = {}
+
+    def has_pillarenv(self):
+        with open('/etc/salt/grains', 'r') as f:
+            stack_name = "{}".format("".join([s for s in f.readlines() if 'opg_stackname' in s]))
+        opg_stack = stack_name.split(':')
+        with open('/etc/salt/minion', 'r') as f:
+            stack_name = "{}".format("".join([s for s in f.readlines() if 'pillarenv'in s]))
+        pillar_env = stack_name.split(':')
+        return opg_stack[1] == pillar_env[1]
 
 
     def complete(self):
@@ -42,14 +53,13 @@ class bootstrapTest(object):
         except IOError:
             return False
 
-
     def check_services(self):
         """
         Set value of service_state dict based on commands run
         """
         for service in self.services[test_type]:
             use_systemd = os.path.exists('/etc/systemd/')
-            check_cmd = [ '/usr/sbin/service', service, 'status' ]
+            check_cmd = ['/usr/sbin/service', service, 'status']
             try:
                 print "{}\n".format(check_cmd)
                 svc = subprocess.check_output(check_cmd)
@@ -77,7 +87,7 @@ if __name__ == "__main__":
     # end_filename = 'hosts'
     end_filename = '/var/run/opg-user-data-done'
     MSG = 'Bootstrap failed'
-    boot = bootstrapTest(end_filename, test_type)
+    boot = BootstrapTest(end_filename, test_type)
     if boot.complete():
         boot.check_services()
         print boot.service_state
@@ -85,4 +95,5 @@ if __name__ == "__main__":
             MSG = "Bootstrap complete"
         else:
             print "{}\n".format(boot.error)
+    assert boot.has_pillarenv()
     print "{}\n".format(MSG)
