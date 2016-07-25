1ca026e364751a233bf6ab8a14abf5b11fdd326a
Tim McArthur <tim.mcarthur@digital.justice.gov.uk>
2016-07-25 15:53:57 +0100

    Remove invalid loop device opts

--- modules/20-docker.sh
+++ modules/20-docker.sh
@@ -3,11 +3,6 @@
 if grep -q '\W/srv\W*btrfs' /etc/fstab
 then
     echo "DOCKER_OPTS=\"\${DOCKER_OPTS} -s btrfs\"" >> /etc/default/docker
-
-else
-    echo "DOCKER_OPTS=\"\${DOCKER_OPTS} --storage-opt dm.no_warn_on_loop_devices=true\"" >> /etc/default/docker
-    # Use "btrfs" over "aufs" by default as the solution
-    # for the Copy-on-Write (CoW) file system.
 fi
 
 service docker restart
