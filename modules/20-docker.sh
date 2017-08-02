b0a53aeef79e26b1cdf17e8c7cec891db6db35b3
Azul <mail@azulinho.com>
2017-08-02 11:28:32 +0100

    wipe out branch

--- modules/20-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if grep -q '\W/srv\W*btrfs' /etc/fstab
-then
-    echo "DOCKER_OPTS=\"\${DOCKER_OPTS} -s btrfs\"" >> /etc/default/docker
-fi
-
-service docker restart
--- modules/99-end.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-touch /var/run/opg-user-data-done
-echo "END: $(TZ=UTC date)"
